b68b390038eb50db59d8e1b164ff65e3f96d5080
林锦隆 <ljl@meitu.com>
2015-10-09 18:06:31 +0800

    [add] 添加GSONjar

--- app/src/main/java/com/demo/qiniu/MainActivity.java
+++ app/src/main/java/com/demo/qiniu/MainActivity.java
@@ -2,6 +2,7 @@ package com.demo.qiniu;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
